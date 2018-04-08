f91154b7e22b08736660c7c2336324120f08f3c8
Karunakar reddy <S530472@nwmissouri.edu>
2018-04-08 13:56:39 -0500

    Updated mapper.py and reducer.py. Drawn the bar graph

--- CrimeDistrict/mapper.py
+++ CrimeDistrict/mapper.py
@@ -1,11 +1,11 @@
-f = open('crimedata.txt','r')
-o = open('output.txt','w')
+f = open('crimedata.txt','r') #Opening crimedata.txt in read mode and storing it in file f
+o = open('output.txt','w') #Opening output.txt in write mode and storing it in file o
 
 for data in f:
-    input = data.strip().split(',')
+    input = data.strip().split(',') #Striping the data and splitting the data columns with comma(,) separated
     if (len(input) == 15):    
        crimeDate,crimeTime,crimeCode,location,description,inside_outside,weapon,post,district,neighborhood,longitude,latitude,location,premise,total_incidents = input
-       o.write(district + "\t" + total_incidents +"\n")
+       o.write(district + "\t" + total_incidents +"\n") #From the data columns taking out the district and total_incidents and writing it into file o
 
-f.close()
-o.close()
+f.close() #Closes file f
+o.close() #Closes file o
--- CrimeDistrict/reducer.py
+++ CrimeDistrict/reducer.py
@@ -4,27 +4,36 @@ reducedFile = open("reducedData.txt", "w")
 thisKey = ""
 thisValue = 0
 values = []
+value = []
 valuesnums = []
 stri = " "
-
+#for loop iterates the sorted file and takes out the district,crimes out of it and separates them with tab space
 for line in sortedFile:
     data = line.strip().split('\t')
     #print data
     district, crimes = data
     if district != thisKey:
-        if thisKey:
+        if thisKey !="":
             reducedFile.write(thisKey + '\t' + str(thisValue)+'\n')
 
         thisKey = district
         thisValue = 0
     # valuesnums.append(district+" "+str(crimes))
     values.append(crimes)
+    value.append(district+": "+str(crimes))
     # print values
 minimum=min(values)
 maximum=max(values)
 
-print "min number of crimes: "+minimum
-print "max number of crimes: "+maximum
+# print "min number of crimes: "+minimum
+# print "max number of crimes: "+maximum
+#For loop checks the value range based on maximum and minimum
+for x in range(len(value)):
+    # print value[x]
+    if value[x].endswith(minimum):
+        print "Minimum number of crimes in the district "+value[x]
+    if value[x].endswith(maximum):
+        print "Maximum number of crimes in the district "+value[x]
 
 sortedFile.close()
 reducedFile.close()
