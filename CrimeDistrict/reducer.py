02a65f27a53d2772ac4df55a1f71c4b46c281ce6
Karunakar reddy <S530472@nwmissouri.edu>
2018-04-11 09:03:12 -0500

    Previous version of reducer has lists. Updated the reducer without lists

--- CrimeDistrict/reducer.py
+++ CrimeDistrict/reducer.py
@@ -3,37 +3,39 @@ reducedFile = open("reducedData.txt", "w")
 
 thisKey = ""
 thisValue = 0
-values = []
-value = []
-valuesnums = []
-stri = " "
+count = 0
+minimum=0
+maximum=0
+maxDistrict=""
+minDistrict=""
 #for loop iterates the sorted file and takes out the district,crimes out of it and separates them with tab space
 for line in sortedFile:
     data = line.strip().split('\t')
-    #print data
     district, crimes = data
     if district != thisKey:
-        if thisKey !="":
-            reducedFile.write(thisKey + '\t' + str(thisValue)+'\n')
-
+        if thisKey !="" and thisKey:
+            reducedFile.write(thisKey + '\t' + str(minimum)+'\t' + str(maximum)+'\n')
+            count = count + 1
         thisKey = district
         thisValue = 0
-    # valuesnums.append(district+" "+str(crimes))
-    values.append(crimes)
-    value.append(district+": "+str(crimes))
-    # print values
-minimum=min(values)
-maximum=max(values)
-
-# print "min number of crimes: "+minimum
-# print "max number of crimes: "+maximum
-#For loop checks the value range based on maximum and minimum
-for x in range(len(value)):
-    # print value[x]
-    if value[x].endswith(minimum):
-        print "Minimum number of crimes in the district "+value[x]
-    if value[x].endswith(maximum):
-        print "Maximum number of crimes in the district "+value[x]
+        minValue=0
+        maxValue=0
+    # Comparing each of the crimes with previous value if it is greater then the maximum value is set the present value
+    # And also the district is stored into the string
+    if(crimes>maximum):
+        maximum=crimes
+        maxDistrict=district
+    # In the below step we are initializing the minimum value for the first time with one of the values in the records
+    # Since minimum value cannot be initialized to aero for comparision purpose
+    if count == 0:
+        minimum = crimes
+    # After initializing the minimum value we start comparing the present values with the minimum value
+    # and if it less than the previous value then minimum value is assigned with present value
+    elif (crimes<minimum):
+        minimum=crimes
+        minDistrict=district
+print 'Minimum number of crimes are found in '+minDistrict+' district with a count of'+str(minimum)+' crimes'
+print 'Maximum number of crimes are found in '+maxDistrict+' district with a count of '+str(maximum)+' crimes'
 
 sortedFile.close()
 reducedFile.close()
